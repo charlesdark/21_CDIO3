a2bc99fcb6217d78b76a7bd4f5b7726af482fbb9
SimonP2602 <91070384+SimonP2602@users.noreply.github.com>
2021-11-09 16:54:40 +0100

    Tilføjet vilkårlighed ud fra 2-4 spillere med array. Herved tilføjet pengebeholdning java klasse.

--- src/main/java/MonopolyJunior.java
+++ src/main/java/MonopolyJunior.java
@@ -1,3 +1,36 @@
+import java.util.Scanner;
+
 public class MonopolyJunior {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Dice dice = new Dice();
+        String name;
+        int Max;
+
+        System.out.println("2-4 spillere.");
+        System.out.println("Hvor mange spiller?: ");
+        Max = sc.nextInt();
+        String spillere[] = new String[Max];
+        int Beholdning[] = new int[Max];
+
+
+       for(int i = 0; i < Max; i++){
+            sc.nextLine();
+            Spiller spiller = new Spiller();
+            System.out.println("Indtast navn: ");
+            spiller.name = sc.nextLine();
+            spillere[i] = spiller.name;
+            Pengebeholdning pengebeholdning = new Pengebeholdning();
+            Beholdning[i] = pengebeholdning.getSaldo();
+        }
+       Beholdning[2] += 500;
+       Beholdning[1] += 1000;
+
+       for (int i = 0; i < Max; i++) {
+           System.out.println("Spiller: " + spillere[i] + ", slå med terningerne!");
+           System.out.println("Spiller " + spillere[i] + ", din saldo er: " + Beholdning[i]);
+       }
+
+    }
 
 }
