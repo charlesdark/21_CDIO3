750602bc0f1b408f4e0ffa4e6b15d335d853deba
SimonP2602 <91070384+SimonP2602@users.noreply.github.com>
2021-11-22 15:43:08 +0100

    Opdateret spillepladens udseende

--- src/main/java/MonopolyJunior.java
+++ src/main/java/MonopolyJunior.java
@@ -10,31 +10,30 @@ public class MonopolyJunior {
 
         //Opretter en spilleplade med 40 felter
         GUI_Field[] fields = new GUI_Field[24];
-        GUI_Start start = new GUI_Start(); start.setTitle("GO!"); start.setBackGroundColor(Color.GREEN); start.setSubText("Modtag 2$"); fields[0] = start;
-        GUI_Street street1 = new GUI_Street(); street1.setTitle("Burger Joint"); street1.setBorder(Color.BLACK); street1.setBackGroundColor(Color.RED); street1.setSubText("2$"); fields[1] = street1;
-        GUI_Street street2 = new GUI_Street(); street2.setTitle("Pizza Joint"); street2.setBorder(Color.BLACK); street2.setBackGroundColor(Color.RED); street2.setSubText("2$"); fields[2] = street2;
+        fields[0] = new GUI_Start("GO!", "Du modtager 2$", "Du har passeret start.", Color.GREEN, Color.BLACK);
+        fields[1] = new GUI_Street("Burger Bar", "2$", "Du har været ude og købe en burger til aftensmad, den koster dig 2$", "2$", Color.RED, Color.BLACK);
+        fields[2] = new GUI_Street("Pizza bar", "2$", "Du har været ude og købe en pizza til aftensmad, den koster dig 2$", "2$", Color.RED, Color.BLACK);
         GUI_Chance chance = new GUI_Chance(); chance.setBackGroundColor(Color.GREEN); chance.setForeGroundColor(Color.BLACK); fields[3] = chance;
-        GUI_Street street3 = new GUI_Street(); street3.setTitle("Candy Store"); street3.setBorder(Color.BLACK); street3.setBackGroundColor(Color.YELLOW); street3.setSubText("2$"); fields[4] = street3;
-        GUI_Street street4 = new GUI_Street(); street4.setTitle("Ice Cream Palour"); street4.setBorder(Color.BLACK); street4.setBackGroundColor(Color.YELLOW); street4.setSubText("2$"); fields[5] = street4;
-        GUI_Street pay = new GUI_Street(); pay.setTitle("pay 2$ dollars"); pay.setBorder(Color.BLACK); pay.setBackGroundColor(Color.GREEN); pay.setSubText("2$"); fields[6] = pay;
-        GUI_Street street5 = new GUI_Street(); street5.setTitle("Museum"); street5.setBorder(Color.BLACK); street5.setBackGroundColor(Color.BLUE); street5.setSubText("2$"); fields[7] = street5;
-        GUI_Street street6 = new GUI_Street(); street6.setTitle("Library"); street6.setBorder(Color.BLACK); street6.setBackGroundColor(Color.BLUE); street6.setSubText("2$"); fields[8] = street6;
+        fields[4] = new GUI_Street("Slikbutik", "2$", "Du har blandet slik for 2$","2$", Color.YELLOW, Color.BLACK);
+        fields[5] = new GUI_Street("Isbutik", "2$", "Du har købt en is til 2$", "2$", Color.YELLOW, Color.BLACK);
+        fields[6] = new GUI_Shipping("src/main/Ressources/Pay.png", "Betal 2$!", "2$", "Du skal betale 2$ til Loose Change!", "2$", Color.GREEN, Color.BLACK);
+        fields[7] = new GUI_Street("Museum", "2$", "Du har været en tur på museum, og det har kostet dig 2$ for billetten", "2$", Color.BLUE, Color.BLACK);
+        fields[8] = new GUI_Street("Bibliotek", "2$", "Du har været en tur på biblioteket, og det har kostet dig 2$ for at få et bibliotekskort", "2$", Color.BLUE, Color.BLACK);
         GUI_Chance chance2 = new GUI_Chance(); chance2.setBackGroundColor(Color.GREEN); chance2.setForeGroundColor(Color.BLACK); fields[9] = chance2;
-        GUI_Street street7 = new GUI_Street(); street7.setTitle("Skate Park"); street7.setBorder(Color.BLACK); street7.setBackGroundColor(Color.ORANGE); street7.setSubText("2$"); fields[10] = street7;
-        GUI_Street street8 = new GUI_Street(); street8.setTitle("Swimming Pool"); street8.setBorder(Color.BLACK); street8.setBackGroundColor(Color.ORANGE); street8.setSubText("2$"); fields[11] = street8;
-        fields[12] = new GUI_Shipping("src/main/Ressources/train-icon-21.jpg.png", "Railroad","2$","Du er landet på togstationen, og får nu en gratis tur med toget.", "2$", Color.GREEN, Color.BLACK);
-        GUI_Street street9 = new GUI_Street(); street9.setTitle("Video Game Arcade"); street9.setBorder(Color.BLACK); street9.setBackGroundColor(Color.GRAY); street9.setSubText("2$"); fields[13] = street9;
-        GUI_Street street10 = new GUI_Street(); street10.setTitle("Movie Theater"); street10.setBorder(Color.BLACK); street10.setBackGroundColor(Color.GRAY); street10.setSubText("2$"); fields[14] = street10;
-        GUI_Street restrooms = new GUI_Street(); restrooms.setTitle("Restrooms"); restrooms.setBorder(Color.BLACK); restrooms.setBackGroundColor(Color.GREEN); restrooms.setSubText("?"); fields[15] = restrooms;
-        GUI_Street street11 = new GUI_Street(); street11.setTitle("Toy Store"); street11.setBorder(Color.BLACK); street11.setBackGroundColor(Color.CYAN); street11.setSubText("2$"); fields[16] = street11;
-        GUI_Street street12 = new GUI_Street(); street12.setTitle("Pet Store"); street12.setBorder(Color.BLACK); street12.setBackGroundColor(Color.CYAN); street12.setSubText("2$"); fields[17] = street12;
-        GUI_Street loosechange = new GUI_Street(); loosechange.setTitle("Loose Change"); loosechange.setBorder(Color.BLACK); loosechange.setBackGroundColor(Color.GREEN); loosechange.setSubText("JACKPOT!"); fields[18] = loosechange;
-        GUI_Street street13 = new GUI_Street(); street13.setTitle("Bowling Ally"); street13.setBorder(Color.BLACK); street13.setBackGroundColor(Color.PINK); street13.setSubText("2$"); fields[19] = street13;
-        GUI_Street street14 = new GUI_Street(); street14.setTitle("The Zoo"); street14.setBorder(Color.BLACK); street14.setBackGroundColor(Color.PINK); street14.setSubText("2$"); fields[20] = street14;
+        fields[10] = new GUI_Street("Skate park", "2$", "Du har været ude og skate og mangler et nyt skatebræt, det koster dig 2$", "2$", Color.ORANGE, Color.BLACK);
+        fields[11] = new GUI_Street("Svømmehal", "2$", "Du har været en tur i svømmehallen, og det koster dig 2$.", "2$", Color.ORANGE, Color.BLACK);
+        fields[12] = new GUI_Shipping("src/main/Ressources/Railroad.png", "Railroad","2$","Du er landet på togstationen, og får nu en gratis tur med toget.", "2$", Color.GREEN, Color.BLACK);
+        fields[13] = new GUI_Street("Spillehal", "2$", "Du har været i spillehallen med dine venner, du har brugt 2$", "2$", Color.GRAY,Color.BLACK);
+        fields[14] = new GUI_Street("Biografen", "2$", "Du har været i biografen og set en film, billetten kostede 2$", "2$", Color.GRAY, Color.BLACK);
+        fields[15] = new GUI_Shipping("src/main/Ressources/Restrooms.png", "Toilet", "2$", "Du har benyttet dig af et offentligt toilet, og du skal betale 3$ til Loose Change", "2$", Color.GREEN, Color.BLACK);
+        fields[16] = new GUI_Street("Legetøjsbutik", "2$", "Du har været ude og købe legetøj for 2$", "2$", Color.CYAN, Color.BLACK);
+        fields[17] = new GUI_Street("Dyrehandler", "2$", "Du har købt et nyt kæledyr, det kostede dig 2$", "2$", Color.CYAN, Color.BLACK);
+        fields[18] = new GUI_Shipping("src/main/Ressources/dollarSign.png", "Loose Change", "", "Tillykke, du modtager Loose Change!", "2$", Color.GREEN, Color.BLACK);
+        fields[19] = new GUI_Street("Bowlinghal", "2$", "Du har været ude og bowle, det kostede dig 2$", "2$", Color.PINK, Color.BLACK);
+        fields[20] = new GUI_Street("Zoo", "2$", "Du har været i zoologisk have, billetten kostede 2$", "2$", Color.PINK, Color.BLACK);
         GUI_Chance chance3 = new GUI_Chance(); chance3.setBackGroundColor(Color.GREEN); chance3.setForeGroundColor(Color.BLACK); fields[21] = chance3;
-        GUI_Street street15 = new GUI_Street(); street15.setTitle("Park Place"); street15.setBorder(Color.BLACK); street15.setBackGroundColor(Color.BLUE); street15.setSubText("2$"); fields[22] = street15;
-        GUI_Street street16 = new GUI_Street(); street16.setTitle("BoardWalk"); street16.setBorder(Color.BLACK); street16.setBackGroundColor(Color.BLUE); street16.setSubText("2$"); fields[23] = street16;
-
+        fields[22] = new GUI_Street("Kongens Have", "2$", "Du har været en tur i parken hvor du købte noget at spise, det kostede 2$", "2$", Color.BLUE, Color.BLACK);
+        fields[23] = new GUI_Street("Stadion", "2$", "Du har været ude og se fodbold, det kostede 2$ at komme ind", "2$", Color.BLUE, Color.BLACK);
 
         GUI gui = new GUI(fields);
         gui.setDie(1);
