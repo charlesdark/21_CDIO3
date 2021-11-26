1d4cc5283608f3191bec1f7830d671f996e877b0
charlesdark <s215720@dtu.dk>
2021-11-26 12:44:03 +0100

    fixet chancekort2 fixet at man skulle trykke ok to gange fixet at den rigtige vinder findes

--- src/main/java/MonopolyJunior.java
+++ src/main/java/MonopolyJunior.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.io.IOException;
+
 import gui_fields.*;
 import gui_main.GUI;
 
@@ -25,9 +26,13 @@ public class MonopolyJunior {
         GUI_Player[] player = new GUI_Player[numPlayers];
         Spiller[] spiller = new Spiller[numPlayers];
 
-        if(numPlayers == 2){balance = 20;}
-        else if(numPlayers == 3){balance = 18;}
-        else{balance = 16;}
+        if (numPlayers == 2) {
+            balance = 20;
+        } else if (numPlayers == 3) {
+            balance = 18;
+        } else {
+            balance = 16;
+        }
 
         //Opretter en spiller m. tilhørende bil
         for (int i = 0; i < numPlayers; i++) {
@@ -49,7 +54,7 @@ public class MonopolyJunior {
                 spiller[i].setKonto(balance);
                 gui.addPlayer(player[i]);
             }
-            if (i == 2){
+            if (i == 2) {
                 spiller[i] = new Spiller();
                 String player3In = gui.getUserString("Indtast et navn: ");
                 GUI_Car car3 = new GUI_Car();
@@ -58,7 +63,7 @@ public class MonopolyJunior {
                 spiller[i].setKonto(balance);
                 gui.addPlayer(player[i]);
             }
-            if (i == 3){
+            if (i == 3) {
                 spiller[i] = new Spiller();
                 String player4In = gui.getUserString("Indtast et navn: ");
                 GUI_Car car4 = new GUI_Car();
@@ -70,11 +75,11 @@ public class MonopolyJunior {
         }
 
         //Tilføjer spillerne til start-pladsen
-        for(int i = 0; i < numPlayers; i++) {
+        for (int i = 0; i < numPlayers; i++) {
             gameboard.fields[0].setCar(player[i], true);
         }
 
-        for(int i = 0; player[i].getBalance() > 0; i++){
+        for (int i = 0; player[i].getBalance() > 0; i++) {
             //Slår med terningen
             gui.showMessage(player[i].getName() + " tryk OK for at slå med terningen");
             spiller[i].roll();
@@ -88,58 +93,59 @@ public class MonopolyJunior {
             //Sætter værdien af feltet i Felter, for at at kunne indlæse den rigtige linje fra tekstfiler
             felter.setFelt(spiller[i].getFelt());
             gameboard.fields[spiller[i].getFelt()].setCar(player[i], true);
-            gui.showMessage(felter.getFeltstr());
+
 
             //Opdaterer spillerens konto og sender personen i fængsel.
-            if(spiller[i].getFelt() == 18){
+            if (spiller[i].getFelt() == 18) {
                 gameboard.fields[spiller[i].getFelt()].removeAllCars();
                 spiller[i].goToJail();
                 gameboard.fields[spiller[i].getFelt()].setCar(player[i], true);
-            }
-            else if(spiller[i].getFelt() == 3 || spiller[i].getFelt() == 9 || spiller[i].getFelt() == 15 || spiller[i].getFelt() == 21){
-               Random chance = new Random();
+            } else if (spiller[i].getFelt() == 3 || spiller[i].getFelt() == 9 || spiller[i].getFelt() == 15 || spiller[i].getFelt() == 21) {
+                Random chance = new Random();
                 int a = chance.nextInt(6);
 
-                if(Objects.equals(chancekort.kortBunke[a].getAction(), "money")){
-                spiller[i].setKonto(chancekort.kortBunke[a].getValue());
-                gui.showMessage(chancekort.kortBunke[a].getDescription());
-               }
-               else if(Objects.equals(chancekort.kortBunke[a].getAction(), "move")){
-                   gameboard.fields[spiller[i].getFelt()].removeAllCars();
-                   spiller[i].setChanceFelt(chancekort.kortBunke[a].getValue());
-                   gui.showMessage(chancekort.kortBunke[a].getDescription());
-                   gameboard.fields[spiller[i].getFelt()].setCar(player[i], true);
-               }
-               else {gameboard.fields[spiller[i].getFelt()].removeAllCars();
+                if (Objects.equals(chancekort.kortBunke[a].getAction(), "money")) {
+                    spiller[i].setKonto(chancekort.kortBunke[a].getValue());
+                    gui.showMessage(chancekort.kortBunke[a].getDescription());
+                } else if (Objects.equals(chancekort.kortBunke[a].getAction(), "move")) {
+                    gameboard.fields[spiller[i].getFelt()].removeAllCars();
+                    spiller[i].setChanceFelt(chancekort.kortBunke[a].getValue());
+                    gui.showMessage(chancekort.kortBunke[a].getDescription());
+                    gameboard.fields[spiller[i].getFelt()].setCar(player[i], true);
+                } else {
+                    gameboard.fields[spiller[i].getFelt()].removeAllCars();
                     spiller[i].moveToStart();
                     gui.showMessage(chancekort.kortBunke[a].getDescription());
-                    gameboard.fields[spiller[i].getFelt()].setCar(player[i], true);}
+                    gameboard.fields[spiller[i].getFelt()].setCar(player[i], true);
+                }
             }
 
             //Opdaterer spilleren konto
-            else {spiller[i].setKonto(felter.getFeltInt());
-                gui.showMessage(felter.getFeltstr());}
+            else {
+                spiller[i].setKonto(felter.getFeltInt());
+                gui.showMessage(felter.getFeltstr());
+            }
             player[i].setBalance(spiller[i].getKonto());
 
 
             //Tjekker om spillerens konto er under 1.
-            if(player[i].getBalance() < 1) {
+            if (player[i].getBalance() < 1) {
                 break;
-            }
-            else if (i == (numPlayers - 1)){
+            } else if (i == (numPlayers - 1)) {
                 i = -1;
             }
-            }
-
-        int max = player[0].getBalance();
+        }
+        int max = spiller[0].getKonto();
+        String vinder = player[0].getName();
         int i;
 
         // Vurderer hvilken af spillerne har den højeste pengebeholdning efter en er gået fallit.
-        for (i = 0; i < numPlayers - 1; i++) {
-            if (player[i].getBalance() > max) {
-                max = player[i].getBalance();
+        for (i = 0; i < numPlayers; i++) {
+            if (spiller[i].getKonto() > max) {
+                max = spiller[i].getKonto();
+                vinder = player[i].getName();
             }
         }
-        gui.showMessage("Vinderen er " + player[i].getName());
+        gui.showMessage("Vinderen er " + vinder);
     }
 }
--- src/main/java/chanceKortHandler.java
+++ src/main/java/chanceKortHandler.java
@@ -2,7 +2,7 @@ public class chanceKortHandler extends Chancekort{
     Chancekort[] kortBunke = new Chancekort[10];
     public chanceKortHandler(){
         Chancekort kort1 = new Chancekort("Ryk 5 felter frem!", "move", 5); kortBunke[0] = kort1;
-        Chancekort kort2 = new Chancekort("Modtag $2!", "money", 10); kortBunke[1] = kort2;
+        Chancekort kort2 = new Chancekort("Modtag $2!", "money", 2); kortBunke[1] = kort2;
         Chancekort kort3 = new Chancekort("Du har har vundet skolelotteriet, du modtager $10!", "money", 10); kortBunke[2] = kort3;
         Chancekort kort4 = new Chancekort("Ryk til start!", "moveToStart", 0); kortBunke[3] = kort4;
         Chancekort kort5 = new Chancekort("Du skal betale $5 i renter!", "money", -5); kortBunke[4] = kort5;
